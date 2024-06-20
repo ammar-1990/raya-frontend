fcad9b0f915d536e19e84e9ceae66d1f664ca6b3
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-20 16:52:28 +0400

    color and icon fix

--- types.tsx
+++ types.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from "react"
 import { LiaLaptopCodeSolid } from "react-icons/lia";
 import { Bot, Globe, Rocket } from "lucide-react";
+import { $Enums } from "@prisma/client";
 
 //bg-[#D4F0FF]
 //bg-[#FFDED4]
 //bg-[#FFEED4]
 
 
-type IconsType = "SOFTWARE" | "DESIGN" | "AI" | "MARKETING"
-type ColorsType = "LIGHT_PINK" | "LIGHT_YELLOW" | "LIGHT_BLUE"
+// export type IconsType = "SOFTWARE" | "DESIGN" | "AI" | "MARKETING"
+// export type ColorsType = "LIGHT_PINK" | "LIGHT_YELLOW" | "LIGHT_BLUE"
 
 
-export const iconsMapping: { [key in IconsType]: ReactNode } = {
+export const iconsMapping: { [key in $Enums.Icons]: ReactNode } = {
     SOFTWARE: <LiaLaptopCodeSolid strokeWidth={0.01} size={30} />,
     AI: <Bot strokeWidth={1.5} size={25} />,
     DESIGN: <Globe strokeWidth={1.5} size={25} />,
@@ -19,7 +20,7 @@ export const iconsMapping: { [key in IconsType]: ReactNode } = {
   };
 
   
-export const colorsMapping: { [key in ColorsType]: string } = {
+export const colorsMapping: { [key in $Enums.Colors]: string } = {
   LIGHT_BLUE: "bg-[#D4F0FF]",
   LIGHT_PINK: "bg-[#FFDED4]",
   LIGHT_YELLOW: "bg-[#FFEED4]",
