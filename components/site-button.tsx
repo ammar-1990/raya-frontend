ff0ec1bc7bb73a4e68c55d6fe5810a6debb81c05
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-22 23:19:27 +0400

    blur

--- components/site-button.tsx
+++ components/site-button.tsx
@@ -6,19 +6,19 @@ type Props = {containerStyle?:string} & HTMLAttributes<HTMLButtonElement>
 
 const SiteButton = ({className,children,containerStyle,...rest}: Props) => {
   return (
-    <div className={cn('relative',containerStyle)}>
+    <div className={cn('relative group',containerStyle)}>
         <Button
     variant={'ghost'}
-    className={cn('bg-transparent hover:bg-backGround hover:text-white rounded-full border w-[126px] border-neutral-600 relative bg-backGround z-30 ',className)}
+    className={cn('bg-transparent hover:bg-backGround hover:text-white rounded-full border w-[126px] border-neutral-600 relative bg-backGround z-30  ',className)}
     >
       
       
 
        
         {children}</Button>
-    <span className='absolute right-0 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-20 h-12 -translate-y-[50%] blur-md z-0'/>
-    <span className='absolute -right-1 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-8 h-8 -translate-y-[68%] blur-[12px] z-0'/>
-    <span className='absolute right-0 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-8 h-8 -translate-y-[70%] blur-[4px] z-0'/>
+    <span className='absolute right-0 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-20 h-12 -translate-y-[50%] blur-md transition z-0 group-hover:blur-lg'/>
+    <span className='absolute -right-1 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-8 h-8 -translate-y-[68%] blur-[12px] transition z-0 group-hover:blur-[20px] '/>
+    <span className='absolute right-0 top-1/2 rounded-full  bg-gradient-to-r from-gradient_blur-start to-gradient_blur-end  w-8 h-8 -translate-y-[70%] blur-[4px] transition z-0 group-hover:blur-[10px]'/>
     </div>
     
   )
