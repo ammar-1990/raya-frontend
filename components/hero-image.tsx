a10da4f36256f0295ffbce2ca61a3f6f08ec68ff
ammar <ammar.ali.haidar.1990@gmail.com>
2024-08-04 15:13:23 +0400

    rotate

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -5,8 +5,10 @@ type Props = {}
 
 const HeroImage = (props: Props) => {
   return (
-    <div className='mx-auto max-w-[750px] 3xl:max-w-[1200px] w-full hover:scale-[1.01] transition   overflow-hidden   aspect-[16/11] p-[1.7px] rounded-lg     hidden md:block z-[10] relative'>
-        <div className='bg-conical-gradient from-black via-black to-gradient_main-end  -top-1/2 -left-1/2  absolute w-[200%] h-[200%] animate-spin-slow ' />
+    <div className='mx-auto max-w-[750px] 3xl:max-w-[1200px] w-full hover:scale-[1.01] transition  overflow-hidden    aspect-[16/11] p-[1.7px] rounded-lg     hidden md:block z-[10] relative'>
+        <div className='bg-conical-gradient from-black   from-90% via-gradient_main-start    to-gradient_main-end      -top-1/2 -left-1/2  absolute w-[200%] h-[200%] animate-spin-slow ' />
+   
+     
         <div className='w-full h-full relative rounded-lg overflow-hidden'>
             <Image src={'/hero.png'} alt='hero' fill className='object-cover' />
 
