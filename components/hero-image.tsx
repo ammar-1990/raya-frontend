f6aa3e34e1f782839e191d796ec94037beb171c7
ammar <ammar.ali.haidar.1990@gmail.com>
2024-08-04 13:43:31 +0400

    image

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const HeroImage = (props: Props) => {
   return (
-    <div className='mx-auto max-w-[750px] w-full   aspect-video p-4 rounded-lg bg-zinc-600 border hidden md:block z-[9999] relative'>
+    <div className='mx-auto max-w-[750px] 3xl:max-w-[1200px] w-full   aspect-video p-4 rounded-lg bg-zinc-600 border hidden md:block z-[9999] relative'>
         <div className='w-full h-full relative rounded-lg overflow-hidden'>
             <Image src={'/hero.png'} alt='hero' fill className='object-cover' />
 
