1c4295d4be477866d71aeaf652402b1975b101fb
ammar <ammar.ali.haidar.1990@gmail.com>
2024-08-04 14:13:32 +0400

    conical background

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -5,7 +5,8 @@ type Props = {}
 
 const HeroImage = (props: Props) => {
   return (
-    <div className='mx-auto max-w-[750px] 3xl:max-w-[1200px] w-full hover:scale-[1.01] transition   aspect-[16/11] p-4 rounded-lg bg-zinc-600 border hidden md:block z-[10] relative'>
+    <div className='mx-auto max-w-[750px] 3xl:max-w-[1200px] w-full hover:scale-[1.01] transition   overflow-hidden   aspect-[16/11] p-[1.7px] rounded-lg     hidden md:block z-[10] relative'>
+        <div className='bg-conical-gradient from-gradient_main-start via-white origin-center to-gradient_main-end  -top-1/2 -left-1/2  absolute w-[200%] h-[200%] animate-spin-slow ' />
         <div className='w-full h-full relative rounded-lg overflow-hidden'>
             <Image src={'/hero.png'} alt='hero' fill className='object-cover' />
 
