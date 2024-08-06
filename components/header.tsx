5e78e00d985e051e1341e2016122a094517f0ad5
ammar <ammar.ali.haidar.1990@gmail.com>
2024-08-06 21:36:43 +0400

    logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavLinks from './navLinks'
 import HamburgerSheet from './hamburger-sheet'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -9,7 +10,9 @@ const Header = (props: Props) => {
     <div className='relative z-10'>
 <div className='siteContainer flex items-center justify-between h-[72px]'>
     {/* logo */}
-    <div className='text-white'>Logo</div>
+    <div className='w-12 aspect-video relative cursor-pointer'>
+<Image src={'/raya-logo.png'} fill alt='logo' className='object-contain'/>
+    </div>
 
     <div className='hidden md:block'>
     <NavLinks/>
