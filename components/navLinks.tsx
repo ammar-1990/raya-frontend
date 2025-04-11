61955e9757d8add169160fa6016e8bbf4045fa56
ammar <ammar.ali.haidar.1990@gmail.com>
2025-04-11 20:37:04 +0400

    refine

--- components/navLinks.tsx
+++ components/navLinks.tsx
@@ -1,55 +1,68 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import SiteButton from './site-button'
-import { useContact } from '@/contexts/contact-context'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import SiteButton from "./site-button";
+import { useContact } from "@/contexts/contact-context";
 
-type Props = {}
+type Props = {};
 
 const NavLinks = (props: Props) => {
-
-  const {open,setOpen} = useContact()
-const pathname = usePathname()
-    const links = [
-        {
-        label:"Services",
-        link:"/#services",
-        active:pathname==="#services"
-  },
-        {
-        label:"Contact us",
-      
-  
-  },
-  //       {
-  //       label:"Pricing",
-  //       link:"/pricing",
-  //       active:pathname==="/pricing"
-  // },
-  //       {
-  //       label:"Blog",
-  //       link:"blog",
-  //       active:pathname==="/blog"
-  // },
-]
+  const { open, setOpen } = useContact();
+  const pathname = usePathname();
+  const links = [
+    {
+      label: "Services",
+      link: "/#services",
+      active: pathname === "#services",
+    },
+    {
+      label: "About",
+      link: "/about",
+    },
+    {
+      label: "Contact us",
+      link: "/contact",
+    },
+    //       {
+    //       label:"Pricing",
+    //       link:"/pricing",
+    //       active:pathname==="/pricing"
+    // },
+    //       {
+    //       label:"Blog",
+    //       link:"blog",
+    //       active:pathname==="/blog"
+    // },
+  ];
   return (
     <nav>
-        <ul className='flex items-center gap-16 text-white text-sm'>
-            {links.map((link)=>{
-             
-              return <li key={link.label}>
-                <Link href={link.link ?? ''}   className='tracking-wider font-sans'>{link.label}</Link>
-            </li>})}
-            <li><SiteButton fn={(e: React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
-        e.preventDefault();
-       setOpen(true)
-       console.log(open)
-      }} className='tracking-wider'>Get Started</SiteButton></li>
-        </ul>
+      <ul className="flex items-center gap-16 text-white text-sm">
+        {links.map((link) => {
+          return (
+            <li key={link.label}>
+              <Link href={link.link ?? ""} className="tracking-wider font-sans">
+                {link.label}
+              </Link>
+            </li>
+          );
+        })}
+        <li>
+          <SiteButton
+            fn={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+              e.preventDefault();
+              setOpen(true);
+              console.log(open);
+            }}
+            className="tracking-wider"
+          >
+            Get Started
+          </SiteButton>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavLinks
+export default NavLinks;
